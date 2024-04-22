d2739abefb2894f380370505901af0346f714263
Ingrith Santamaria <ingrithsanta@gmail.com>
2024-04-22 15:18:44 -0500

    Implementation of Soft assertions

--- tests/weatherTest.spec.js
+++ tests/weatherTest.spec.js
@@ -12,45 +12,45 @@ test.describe('OpenWeatherMap API Tests', () => {
   test('The API should return the current weather data for a city', async () => {
     const response = await weatherApi.getWeatherByCityName(context, 'Republic of Guatemala');
     const responseBody = await response.json();
-    test.expect(response.status()).toBe(200);
-    test.expect(responseBody).toHaveProperty('weather');
-    test.expect(responseBody).toHaveProperty('main');
+    test.expect.soft(response.status()).toBe(200);
+    test.expect.soft(responseBody).toHaveProperty('weather');
+    test.expect.soft(responseBody).toHaveProperty('main');
   });
   test('The API should retrieve weather data using a city ID', async () => {
     const response = await weatherApi.getWeatherByCityId(context, '3563856');
     const responseBody = await response.json();
-    test.expect(response.status()).toBe(200);
-    test.expect(responseBody).toHaveProperty('weather');
-    test.expect(responseBody).toHaveProperty('main');
+    test.expect.soft(response.status()).toBe(200);
+    test.expect.soft(responseBody).toHaveProperty('weather');
+    test.expect.soft(responseBody).toHaveProperty('main');
   });
   test('The API should be able to return a 5-day forecast for a specific city', async () => {
     const response = await weatherApi.getForecastByCityName(context, 'España');
     const responseBody = await response.json();
     console.log(JSON.stringify(responseBody))
-    test.expect(response.status()).toBe(200);
-    test.expect(responseBody).toHaveProperty('list');
-    test.expect(responseBody.list.length).toBeGreaterThan(0);
-    test.expect(response.body.list).to.have.length(40);
+    test.expect.soft(response.status()).toBe(200);
+    test.expect.soft(responseBody).toHaveProperty('list');
+    test.expect.soft(responseBody.list.length).toBeGreaterThan(0);
+    test.expect.soft(response.body.list).to.have.length(40);
   });
   test('Should validate the structure of the weather response for a city', async () => {
     const response = await weatherApi.getForecastByCityName(context, 'Paris');
     const responseBody = await response.json();
     console.log(JSON.stringify(responseBody))
-    test.expect(response.status()).toBe(200);
+    test.expect.soft(response.status()).toBe(200);
     // console.log(JSON.stringify(response.status()))
-    test.expect(responseBody.list[0]).toHaveProperty('weather');
-    test.expect(responseBody.list[0]).toHaveProperty('main');
-    test.expect(responseBody.list[0]).toHaveProperty('wind');
-    test.expect(responseBody.name).not.toBeNull();
-    test.expect(responseBody.list[0].weather).toBeInstanceOf(Array);
-    test.expect(responseBody.list[0].weather.length).toBeGreaterThan(0);
-    test.expect(responseBody.list[0].main).not.toBeNull();
-    test.expect(responseBody.list[0].wind).not.toBeNull();
-    // test.expect(typeof responseBody.list[0].name).toBe('string')
+    test.expect.soft(responseBody.list[0]).toHaveProperty('weather');
+    test.expect.soft(responseBody.list[0]).toHaveProperty('main');
+    test.expect.soft(responseBody.list[0]).toHaveProperty('wind');
+    test.expect.soft(responseBody.name).not.toBeNull();
+    test.expect.soft(responseBody.list[0].weather).toBeInstanceOf(Array);
+    test.expect.soft(responseBody.list[0].weather.length).toBeGreaterThan(0);
+    test.expect.soft(responseBody.list[0].main).not.toBeNull();
+    test.expect.soft(responseBody.list[0].wind).not.toBeNull();
+    // test.expect.soft(typeof responseBody.list[0].name).toBe('string')
   })
   test('Should handle 401 Unauthorized for invalid API key', async () => {
     const response = await weatherApi.getWeatherByCityNameInvalid(context, 'España');
     console.log(`Status Code: ${response.status()}`);
-    test.expect(response.status()).toBe(401);
+    test.expect.soft(response.status()).toBe(401);
   })
 });
